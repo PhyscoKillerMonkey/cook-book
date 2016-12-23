673cf2f84e777f03d4a84b4a7352685d969b7023
Reece Bennett <reece.bennett1@gmail.com>
2016-12-23 19:09:11 +0000

    Remove server.ts, just make js file

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 "use strict";
-var App_1 = require("./App");
+var App_1 = require(__dirname + "/dist/App");
+
 // Get a port from the environment or use default
 var port = normalizePort(process.env.PORT || 1337);
+
 // Create the server, passing in the port
 var app = new App_1.App(port);
+
 function normalizePort(val) {
     var port = (typeof val === 'string') ? parseInt(val, 10) : val;
     if (isNaN(port))
